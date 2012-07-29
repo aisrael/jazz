8e1e72c9951d7a95de8f497eaba70a37dcf7f91b
Alistair A. Israel <aisrel@gmail.com>
2012-07-29 15:37:12 +0800

    rails new plugin jazz

---
+++ lib/tasks/jazz_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :jazz do
+#   # Task goes here
+# end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '1ba808814f1186e1d04ebaced9b15fa7ea28b123394d8f4c025bdb723da0019a6ccf7cde955704b4da3494bfcd32a8711864b3566abb37ba153d0b66bb67a770'
