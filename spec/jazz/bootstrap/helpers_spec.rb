4a49640dc9b19e35bb4e36fb00bbf51ee4c85ba0
Alistair A. Israel <aisrel@gmail.com>
2012-09-08 12:15:55 +0800

    Better specs

---
+++ spec/jazz/bootstrap/helpers_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+require 'jazz/bootstrap/helpers'
+
+describe Jazz::Bootstrap::Helpers do
+
+  require 'active_support'
+  require 'action_view'
+  
+  include ActionView::Helpers
+
+  it { is_a? Module }
+
+  include Jazz::Bootstrap::Helpers
+  
+  describe 'thead' do
+    it 'generates a <thead...> tag' do
+      self.should_receive(:t).with(:a, default: 'A').and_return('A')
+      self.should_receive(:t).with(:b, default: 'B').and_return('B')
+      thead(:a, :b).should eq '<thead><tr><th>A</th><th>B</th></tr></thead>'
+    end
+  end
+end
--- spec/jazz/jazz_helper_spec.rb
+++ spec/jazz/jazz_helper_spec.rb
@@ -1,29 +1,29 @@
 require 'spec_helper'
 
-require 'active_record'
-require 'action_view'
-require 'rails-i18n'
-
 require 'jazz_helper'
 
-describe 'JazzHelper' do
+describe JazzHelper do
 
-  include JazzHelper
+  require 'active_support'
+  require 'active_record'
+  require 'action_view'
+  
+  include ActionView::Helpers
   
   it { is_a? Module }
+
+  include JazzHelper
 
   describe 'label_for' do
     it 'generates a <label...> tag' do
       self.should_receive(:t).with(:field, default: 'Field').and_return('Field')
-      self.should_receive(:label_tag).with(:field, 'Field', {}).and_return('<label for="field">Field</label>')
       label_for(:field).should eq('<label for="field">Field</label>')
     end
   end
   
   describe 'link' do
     it 'generates an <a ...> tag' do
-      self.should_receive(:link_to).with('Google', 'http://www.google.com', {title: 'Google'}).and_return('<a href="http://www.google.com">Google</a>')
-      link('Google', 'http://www.google.com').should eq('<a href="http://www.google.com">Google</a>')
+      link('Google', 'http://www.google.com').should eq('<a href="http://www.google.com" title="Google">Google</a>')
     end
   end
 end
