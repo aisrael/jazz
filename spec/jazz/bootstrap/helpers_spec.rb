96223aa82a325270b49e0d04a9c926f737682851
Alistair A. Israel <aisrael@gmail.com>
2012-12-20 12:50:02 +0800

    Simplified bootstrap/helpers_spec

--- spec/jazz/bootstrap/helpers_spec.rb
+++ spec/jazz/bootstrap/helpers_spec.rb
@@ -1,23 +1,10 @@
 require 'spec_helper'
 
-require 'jazz/bootstrap/helpers'
-
-describe Jazz::Bootstrap::Helpers do
-
-  require 'active_support'
-  require 'action_view'
-
-  include ActionView::Helpers
-
-  it { is_a? Module }
-
-  include Jazz::Bootstrap::Helpers
+describe Jazz::Bootstrap::Helpers, type: :helper do
 
   describe 'thead' do
     it 'generates a <thead ...> tag' do
-      self.should_receive(:t).with(:a, default: 'A').and_return('A')
-      self.should_receive(:t).with(:b, default: 'B').and_return('B')
-      thead(:a, :b).should eq '<thead><tr><th>A</th><th>B</th></tr></thead>'
+      helper.thead(:a, :b).should eq '<thead><tr><th>A</th><th>B</th></tr></thead>'
     end
   end
 end
